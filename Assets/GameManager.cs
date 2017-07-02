5fdc6697dd4dd9dd5241928c0949e5091525ec30
tozio23 <t.celata@gmail.com>
2017-07-02 18:35:03 +0200

    race canvas, game logic and engine particles

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -11,16 +11,20 @@ public class GameManager : MonoBehaviour {
     Vector3 StartPosition;
     Vector3 StartRotation;
     public bool isTrack;
+    public GameObject EditorCanvas;
+    public GameObject RaceCanvas;
 
 	// Use this for initialization
 	void Start () {
         isTrack = false;
         TrackCamera = Camera.main;
-
+        
 	}
 	
     public void RaceMode()
     {
+        EditorCanvas.SetActive(false);
+        RaceCanvas.SetActive(true);
         TrackCamera.enabled = false;
         SpaceShip = Instantiate(SpaceShipPrefab, new Vector3(StartPosition.x, StartPosition.y+0.5f, StartPosition.z), Quaternion.LookRotation(StartRotation));
         ShipCamera = SpaceShip.GetComponentInChildren<Camera>();
@@ -29,13 +33,23 @@ public class GameManager : MonoBehaviour {
 
     public void EditorMode()
     {
-
+        EditorCanvas.SetActive(true);
+        RaceCanvas.SetActive(false);
+        Destroy(SpaceShip);
+        TrackCamera.enabled = true;
     }
+
+
 
     public void SetStartPoint(Vector3 position, Vector3 rotation)
     {
         StartPosition = position;
         StartRotation = rotation;
+    }
+
+    public void ResetRace()
+    {
+        SpaceShip.transform.SetPositionAndRotation(new Vector3(StartPosition.x, StartPosition.y + 0.5f, StartPosition.z), Quaternion.LookRotation(StartRotation));
     }
 
 	// Update is called once per frame
--- Assets/SpaceShipMovement.cs
+++ Assets/SpaceShipMovement.cs
@@ -5,26 +5,44 @@ using UnityEngine;
 public class SpaceShipMovement : MonoBehaviour {
 
    float speed = 0f;
-
-  
-
+    public GameObject shipModel;
+    Quaternion originalRotation;
+    GameManager GameManager;
+    public GameObject particleLeft;
+    public GameObject particleRight;
+    Rigidbody rb;
     void Start()
     {
-        //rb = GetComponent<Rigidbody>();
+
+        GameManager = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>();
+        rb = GetComponent<Rigidbody>();
+        originalRotation = shipModel.transform.rotation;
+        //particles.SetActive(false);
     }
 
     void Update()
     {
+        if (transform.position.y < -5f)
+        {
+            GameManager.ResetRace();
+        }
         if (Input.GetButton("Accelerate"))
         {
+            particleLeft.GetComponent<ParticleSystem>().Play();
+            particleRight.GetComponent<ParticleSystem>().Play();
             speed += 0.2f;
         }else if (Input.GetButton("Brake"))
         {
+           
+
             speed -= 0.2f;
         }
         else
         {
-            speed -= 0.1f;
+            particleLeft.GetComponent<ParticleSystem>().Stop();
+            particleRight.GetComponent<ParticleSystem>().Stop();
+            //particles.SetActive(false);
+            speed -= 0.05f;
         }
         float moveHorizontal = Input.GetAxis("Horizontal");
         if (speed < 0)
@@ -35,11 +53,15 @@ public class SpaceShipMovement : MonoBehaviour {
         {
             speed = 1f;
         }
-        
-        //Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
 
-        transform.Rotate(Vector3.up, moveHorizontal*1f);
-        //transform.Rotate(Vector3.forward, moveHorizontal * 0.1f);
+            transform.Rotate(Vector3.up, moveHorizontal * 1.2f);
+
+            Debug.Log(shipModel.transform.rotation.eulerAngles);
+
+
+
+
+        
         transform.Translate(Vector3.forward*(speed*0.1f));
     }
 }
